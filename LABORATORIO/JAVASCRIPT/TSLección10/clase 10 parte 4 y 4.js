b3fba4c52e3cc81ab662d396d6b4879e186fe58c
Orozco Marianela <orozcomarianela10@gmail.com>
2023-07-06 14:38:11 -0300

    Clase 10 Javascript. parte 1.2.3

--- LABORATORIO/JAVASCRIPT/TSLección10/clase 10 parte 4 y 4.js
+++ LABORATORIO/JAVASCRIPT/TSLección10/clase 10 parte 4 y 4.js
@@ -1,9 +1,48 @@
-//Fin de la clase Producto //clase10 parte 3
-Let producto1 =new Producto ("Pantalon",200);
-Let producto2= new Producto("Camisa",150)
-console.log(producto.toStrin()); idProducto:1 ,nombre: Pantalón,precio $200
-console.log(producto2.toString()): idProducto:2, nombre: Camisa, precio: $150
+class Producto{
+    static contadorProductos = 0;
+    constructor(nombre, precio){
+        this._idProducto=++Producto.contadorProductos;//incrementa el valor estatic
+        this._nombre = nombre;
+        this._precio = precio;
+    }
 
+    get idProducto(){
+        return this._idProducto;
+    }
+
+    get nombre(){
+        return this._nombre;
+    }
+
+    set nombre(nombre){
+        this._nombre = nombre;
+    }
+
+    get precio(){
+        return this._precio;
+    }
+
+    set precio(precio){
+        this._precio = precio;
+    }
+    toString() {//Templeate literals : no permite insertar código dinamicamente
+        return `idProducto : ${this._idProducto}, nombre : ${this._nombre}, precio : ${this._precio}`;
+
+    }
+}
+//Fin de la clase Producto //clase10 parte 3
+
+let = producto1 = new Producto("Pantalon", 200);
+let = producto2 = new Producto("Camisa",150);
+console.log(producto1.toString()); idProducto : 1, nombre : Pantalon, precio : $200
+console.log(producto2.toString()); idProducto : 2, nombre : Camisa, precio : $150
+
+/*
+let producto1 = new Producto ("Pantalon",200);
+let producto2 = new Producto("Camisa",150);
+console.log(producto1.toString()); idProducto : 1, nombre : Pantalon, precio : $200
+console.log(producto2.toString()); idProducto: 2, nombre : Camisa, precio : $150
+*/
 
 // clase 10 parte 4
 
@@ -27,14 +66,11 @@ class Orden{
            // this._productos[this._contadorProductoAgregados++] = producto;
             else {
                 console.log("Nose pueden agregar mas productos");
-
             }
         }
         //Fin del metoo agregar producto
 
     }
-
-
 
 }//Fin de la clase orden
 
